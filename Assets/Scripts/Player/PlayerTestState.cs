b63f1b1efc9542cdc11de339105b1a80b3c23441
Paris Hall <32145811+paristopolus@users.noreply.github.com>
2023-06-21 18:29:08 -0400

    Base Player
    Added Emery and Zahra rigs;
    Completed Statemachine progenitors;

--- Assets/Scripts/Player/PlayerTestState.cs
+++ Assets/Scripts/Player/PlayerTestState.cs
@@ -4,24 +4,26 @@ using UnityEngine;
 
 public class PlayerTestState : PlayerBaseState
 {
-    private float timer = 5f;
+    private float timer = 0f;
     public PlayerTestState(PlayerStateMachine stateMachine) : base(stateMachine){}
 
     public override void Enter()
     {
-        Debug.Log("Enter");
+        stateMachine.InputReader.JumpEvent += OnJump;
     }
     public override void Tick(float deltaTime)
     {
-        timer -= deltaTime;
+        timer += deltaTime;
+
         Debug.Log(timer);
-        if(timer <= 0f)
-        {
-            stateMachine.SwitchState(new PlayerTestState(stateMachine));
-        }
     }
     public override void Exit()
     {
-        Debug.Log("Exit");
+        stateMachine.InputReader.JumpEvent -= OnJump;
+    }
+
+    private void OnJump()
+    {
+        stateMachine.SwitchState(new PlayerTestState(stateMachine));
     }
 }
